8ab9cf80c5cd3579eae75d73ddeab1e391a246a9
JuliaNocera <nocera.julia@gmail.com>
2022-05-20 23:31:53 -0700

    refactor: Move circle drawing to own space to work on perlin separately

--- sketch.js
+++ sketch.js
@@ -1,43 +1,19 @@
-// let mic;
-
-// function setup() {
-//   createCanvas(710, 200);
-
-//   // Create an Audio input
-//   mic = new p5.AudioIn();
-
-//   // start the Audio Input.
-//   // By default, it does not .connect() (to the computer speakers)
-//   mic.start();
-// }
-
-// function draw() {
-//   background(200);
-
-//   // Get the overall volume (between 0 and 1.0)
-//   let vol = mic.getLevel();
-//   fill(127);
-//   stroke(0);
-
-//   // Draw an ellipse with height based on volume
-//   let h = map(vol, 0, 1, height, 0);
-//   ellipse(width / 2, h - 25, 50, 50);
-// }
-
-
 const AUDIO_SETTINGS = {
   OFF: 'off',
   ON: 'on'
+}
+
+const VISUALIZATION = {
+  CIRCLE: 'circle',
+  PERLIN: 'perlin'
 }
 
 const BANDS = 64;
 
 let audioIn;
 let currentAudioSetting = AUDIO_SETTINGS.OFF;
+let currentVisualization = VISUALIZATION.CIRCLE;
 let fft;
-// let w;
-
-let volhistory = [];
 
 function startAudio() {
   if(currentAudioSetting === AUDIO_SETTINGS.OFF) {
@@ -50,8 +26,15 @@ function startAudio() {
 
 function setup(){
   createCanvas(800, 800);
-  colorMode(HSB);
-  angleMode(DEGREES);
+
+  if (VISUALIZATION.CIRCLE) {
+    colorMode(HSB);
+    angleMode(DEGREES);
+  }
+
+  if (VISUALIZATION.PERLIN) {
+
+  }
 
   audioIn = new p5.AudioIn();
   audioIn.getSources(gotSources);
@@ -66,7 +49,6 @@ function setup(){
 }
 
 function gotSources(deviceList) {
-  // console.log(deviceList)
   if (deviceList.length > 0) {
     //set the source to the first item in the deviceList array
     audioIn.setSource(0);
@@ -78,24 +60,38 @@ function gotSources(deviceList) {
 function draw() {
   background(0);
   if (currentAudioSetting === AUDIO_SETTINGS.ON) {
-    let spectrum = fft.analyze()
-    noStroke();
-    // translate to the center
-    translate(width / 2, height / 2);
-    // beginShape();
-    for(let i = 0; i < spectrum.length; i++) {
-      let angle = map(i,0,spectrum.length,0,360);
-      let amp = spectrum[i];
-      // set the radius to map that amplitude from 0 --> 256 to 40 --> 200
-      let r = map(amp, 0, BANDS, 40, (width / 6.5));
-      let x = r * cos(angle);
-      let y = r * sin(angle);
-      // vertex(x,y);
-      stroke(amp, 255, 255);
-      line(0, 0, x, y);
+    if (VISUALIZATION.CIRCLE) {
+      drawCircleSpectrum();
     }
-    // endShape();
-    stroke(255);
-    noFill();
+    if (VISUALIZATION.PERLIN) {
+      drawPerlin();
+    }
   }
+}
+
+function drawPerlin() {
+
+}
+
+function drawCircleSpectrum() {
+  let spectrum = fft.analyze()
+  noStroke();
+  // translate to the center
+  translate(width / 2, height / 2);
+
+  for (let i = 0; i < spectrum.length; i++) {
+    let angle = map(i, 0, spectrum.length, 0, 360);
+    let amp = spectrum[i];
+
+    // set the radius to map that amplitude from 0 --> 256 to 40 --> 200
+    let r = map(amp, 0, BANDS, 40, (width / 6.5));
+    let x = r * cos(angle);
+    let y = r * sin(angle);
+
+    stroke(amp, 255, 255);
+    line(0, 0, x, y);
+  }
+
+  stroke(255);
+  noFill();
 }
